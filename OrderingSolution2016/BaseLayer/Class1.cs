e5782ffa6eae780ac1715374a0154b1913db2cc8
larryslack <slack@mhc.ab.ca>
2016-01-22 11:40:38 -0700

    added base layer with customer object so far

---
+++ OrderingSolution2016/BaseLayer/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLayer
+{
+    public class Customer
+    {
+        #region Properties
+        public string CustomerID { get; private set; }
+        public string CompanyName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        #endregion
+
+        #region Constructors
+        public Customer(string CustomerID)
+        {
+            this.CustomerID = CustomerID;
+        }
+
+        public Customer(string CustomerID, string CompanyName, string ContactTitle, string Address, string City, string Region, string PostalCode, string Country, string Phone, string Fax)
+        {
+            this.CustomerID = CustomerID;
+            this.CompanyName = CompanyName;
+            this.ContactTitle = ContactTitle;
+            this.Address = Address;
+            this.City = City;
+            this.Region = Region;
+            this.PostalCode = PostalCode;
+            this.Country = Country;
+            this.Phone = Phone;
+            this.Fax = Fax;
+        }
+        #endregion
+    }
+}
