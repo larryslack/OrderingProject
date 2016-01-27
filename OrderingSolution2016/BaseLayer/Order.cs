271ff8661279392b403f5bcc720d7e32567f46a7
kateyeee <jia.ye@WIN7B228-5.itp.local>
2016-01-27 16:45:49 -0700

    two new files will be committed this time we hope

---
+++ OrderingSolution2016/BaseLayer/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLayer
+{
+    class Order
+    {
+        #region Properties
+        public int? OrderID { get; private set; }
+        public int? CustomerID { get; set; }
+        public int? EmployeeID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime RequiredDate { get; set; }
+
+
+
+
+
+        #endregion
+    }
+}
---
+++ OrderingSolution2016/BaseLayer/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLayer
+{
+    class Product
+    {
+        #region properties
+
+        public int? ProductID { get; private set; }
+        public string ProductName { get; set; }
+        public int? SupplierID { get; set; }
+        public int? CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short? UnitInStock { get; set; }
+        public short? UnitOnOrder { get; set; }
+        public int? ReOrderLevel { get; set; }
+        public bool Discontinued { get; set; }
+        #endregion
+
+        #region Constrction
+
+        public Product(int? ProductID, string ProductName, int? SupplierID, int? CategoryID, string QuantityPerUnit, decimal UnitOrice, short? UnitInStock, short? UnitOnOrder,
+            int? ReOrderLevel, bool Discontinued)
+        {
+            this.ProductID = ProductID;
+            this.ProductName = ProductName;
+            this.SupplierID = SupplierID;
+            this.CategoryID = CategoryID;
+            this.QuantityPerUnit = QuantityPerUnit;
+            this.UnitInStock = UnitInStock;
+            this.UnitOnOrder = UnitOnOrder;
+            this.ReOrderLevel = ReOrderLevel;
+            this.Discontinued = Discontinued;
+        }
+
+
+
+
+
+        #endregion
+
+    }
+}
