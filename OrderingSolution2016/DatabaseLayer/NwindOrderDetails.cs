66e353463389327222c15bda8cb74872f7a12bdf
BeepBoopMeow <brent.perner@mymhc.ca>
2016-02-03 15:48:22 -0700

    Brent's order details (+ stored procedure text file)

---
+++ OrderingSolution2016/DatabaseLayer/NwindOrderDetails.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using BaseLayer;
+
+namespace DatabaseLayer
+{
+    public partial class DB
+    {
+        private const string PROC_ORD_DETAILS = "CustomerOrderDetails";
+
+        public static List<OrderDetail> OrderDetailList(string CustomerID)
+        {
+            //Copied code from Nwind.cs VVVV
+            List<OrderDetail> OrdDetList = new List<OrderDetail>();
+
+            sqlCon = new SqlConnection(connectionString);
+            sqlCon.Open();
+
+            SqlDataAdapter da;
+            DataTable dt = new DataTable();
+            SqlCommand cmd = new SqlCommand(PROC_ORD_DETAILS, sqlCon);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@CustomerID", CustomerID));
+
+            da = new SqlDataAdapter(cmd);
+            da.FillSchema(dt, SchemaType.Source);
+            da.Fill(dt);
+
+            sqlCon.Close();
+            //Copied code^^^^
+            foreach (DataRow row in dt.Rows)
+            {
+                int OrderID = (int)row["OrderID"];
+                int ProductID = (int)row["ProductID"];
+                decimal UnitPrice = (decimal)row["UnitPrice"];
+                short Quantity = (short)row["Quantity"];
+                float Discount = (float)row["Discount"];
+
+                OrderDetail OrD = new OrderDetail(OrderID,ProductID,UnitPrice,Quantity,Discount);
+                OrdDetList.Add(OrD);
+            }
+      /*put code here */
+            return OrdDetList;
+        }
+    }
+}
