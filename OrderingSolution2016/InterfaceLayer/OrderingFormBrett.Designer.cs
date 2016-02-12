671caa7d7fd86f17f42d24f87399596f4bfe28f1
larryslack <slack@mhc.ab.ca>
2016-02-12 11:39:14 -0700

    testing Bretts OrderForm

--- OrderingSolution2016/InterfaceLayer/OrderingFormBrett.Designer.cs
+++ OrderingSolution2016/InterfaceLayer/OrderingFormBrett.Designer.cs
@@ -28,19 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnRandomOrder = new System.Windows.Forms.Button();
+            this.lblCustomerID = new System.Windows.Forms.Label();
+            this.lblEmployeeID = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ls = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // OrderingFromBrett
+            // btnRandomOrder
+            // 
+            this.btnRandomOrder.Location = new System.Drawing.Point(95, 109);
+            this.btnRandomOrder.Name = "btnRandomOrder";
+            this.btnRandomOrder.Size = new System.Drawing.Size(193, 23);
+            this.btnRandomOrder.TabIndex = 0;
+            this.btnRandomOrder.Text = "Make Random Order";
+            this.btnRandomOrder.UseVisualStyleBackColor = true;
+            this.btnRandomOrder.Click += new System.EventHandler(this.btnRandomOrder_Click);
+            // 
+            // lblCustomerID
+            // 
+            this.lblCustomerID.AutoSize = true;
+            this.lblCustomerID.Location = new System.Drawing.Point(92, 9);
+            this.lblCustomerID.Name = "lblCustomerID";
+            this.lblCustomerID.Size = new System.Drawing.Size(72, 13);
+            this.lblCustomerID.TabIndex = 1;
+            this.lblCustomerID.Text = "lblCustomerID";
+            // 
+            // lblEmployeeID
+            // 
+            this.lblEmployeeID.AutoSize = true;
+            this.lblEmployeeID.Location = new System.Drawing.Point(92, 44);
+            this.lblEmployeeID.Name = "lblEmployeeID";
+            this.lblEmployeeID.Size = new System.Drawing.Size(74, 13);
+            this.lblEmployeeID.TabIndex = 2;
+            this.lblEmployeeID.Text = "lblEmployeeID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "CustomerID:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "EmployeeID:";
+            // 
+            // ls
+            // 
+            this.ls.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ls.FormattingEnabled = true;
+            this.ls.Location = new System.Drawing.Point(12, 184);
+            this.ls.Name = "ls";
+            this.ls.Size = new System.Drawing.Size(486, 212);
+            this.ls.TabIndex = 5;
+            // 
+            // OrderingFormBrett
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(510, 408);
-            this.Name = "OrderingFromBrett";
+            this.Controls.Add(this.ls);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblEmployeeID);
+            this.Controls.Add(this.lblCustomerID);
+            this.Controls.Add(this.btnRandomOrder);
+            this.Name = "OrderingFormBrett";
             this.Text = "OrderingFromBrett";
+            this.Load += new System.EventHandler(this.OrderingFormBrett_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRandomOrder;
+        private System.Windows.Forms.Label lblCustomerID;
+        private System.Windows.Forms.Label lblEmployeeID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox ls;
     }
 }
--- OrderingSolution2016/InterfaceLayer/OrderingFormBrett.cs
+++ OrderingSolution2016/InterfaceLayer/OrderingFormBrett.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BaseLayer;
+using BusinessLayer;
 
 namespace InterfaceLayer
 {
     public partial class OrderingFormBrett : Form
     {
+        string CustomerID;
+        int EmployeeID;
+        List<OrderDetail> DetailList = new List<OrderDetail>();
+
         public OrderingFormBrett(string CustomerID, int EmployeeID)
         {
             InitializeComponent();
+            this.CustomerID = CustomerID;
+            this.EmployeeID = EmployeeID;
+            lblCustomerID.Text = CustomerID;
+            lblEmployeeID.Text = EmployeeID.ToString();
+        }
+
+        private void OrderingFormBrett_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnRandomOrder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Order NewOrder = new Order(0, CustomerID, EmployeeID, DateTime.Now, DateTime.Now + new TimeSpan(7, 0, 0, 0), null, null, null, CustomerID + "Name", "123 4 St", "Medicine Hat", "AB", "T1A 7A7", "Canada");
+                Business.SaveOrder(NewOrder);
+                ls.Items.Add("The new order number is " + NewOrder.OrderID.ToString());
+                OrderDetail NewOrderDetail = new OrderDetail(NewOrder.OrderID, 22, 10.22m, 100, 0);
+                DetailList.Add(NewOrderDetail);
+
+                NewOrderDetail = new OrderDetail(NewOrder.OrderID, 33, 33.33m, 200, .2f);
+                DetailList.Add(NewOrderDetail);
+                Business.SaveDetails(NewOrder.OrderID, DetailList);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
         }
     }
 }
