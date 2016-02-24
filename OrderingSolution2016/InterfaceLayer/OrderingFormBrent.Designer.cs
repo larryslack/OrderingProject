0b735f4dc0482674c6350e2f5c051c29dc720263
BeepBoopMeow <brent.perner@mymhc.ca>
2016-02-24 15:26:01 -0700

    Added Brent's form

---
+++ OrderingSolution2016/InterfaceLayer/OrderingFormBrent.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace InterfaceLayer
+{
+    partial class OrderingFormBrent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProductBox = new System.Windows.Forms.ComboBox();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.QuantityUpdown = new System.Windows.Forms.NumericUpDown();
+            this.PriceBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.QuantityUpdown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ProductBox
+            // 
+            this.ProductBox.FormattingEnabled = true;
+            this.ProductBox.Location = new System.Drawing.Point(12, 68);
+            this.ProductBox.Name = "ProductBox";
+            this.ProductBox.Size = new System.Drawing.Size(121, 21);
+            this.ProductBox.TabIndex = 0;
+            this.ProductBox.SelectedIndexChanged += new System.EventHandler(this.ProductBox_SelectedIndexChanged);
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(12, 39);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(75, 23);
+            this.AddBtn.TabIndex = 1;
+            this.AddBtn.Text = "Add";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(260, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(181, 238);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Remove";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Quantity";
+            // 
+            // QuantityUpdown
+            // 
+            this.QuantityUpdown.Location = new System.Drawing.Point(64, 90);
+            this.QuantityUpdown.Name = "QuantityUpdown";
+            this.QuantityUpdown.Size = new System.Drawing.Size(47, 20);
+            this.QuantityUpdown.TabIndex = 7;
+            this.QuantityUpdown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // PriceBox
+            // 
+            this.PriceBox.BackColor = System.Drawing.SystemColors.Info;
+            this.PriceBox.Location = new System.Drawing.Point(49, 120);
+            this.PriceBox.Name = "PriceBox";
+            this.PriceBox.ReadOnly = true;
+            this.PriceBox.Size = new System.Drawing.Size(100, 20);
+            this.PriceBox.TabIndex = 8;
+            this.PriceBox.Text = "0";
+            // 
+            // OrderingFormBrent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 262);
+            this.Controls.Add(this.PriceBox);
+            this.Controls.Add(this.QuantityUpdown);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.ProductBox);
+            this.Name = "OrderingFormBrent";
+            this.Text = "OrderingFormBrent";
+            ((System.ComponentModel.ISupportInitialize)(this.QuantityUpdown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ProductBox;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown QuantityUpdown;
+        private System.Windows.Forms.TextBox PriceBox;
+    }
+}
