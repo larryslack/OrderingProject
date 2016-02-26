b046bd31406a79a7bd1375d12a29876d7a22f1c3
BeepBoopMeow <brent.perner@mymhc.ca>
2016-02-26 11:20:59 -0700

    minor edits to brent form

--- OrderingSolution2016/InterfaceLayer/OrderingFormBrent.cs
+++ OrderingSolution2016/InterfaceLayer/OrderingFormBrent.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using BaseLayer;
 using BusinessLayer;
-//using DatabaseLayer;
 
 namespace InterfaceLayer
 {
@@ -22,6 +21,7 @@ namespace InterfaceLayer
 
         public OrderingFormBrent()
         {
+            //List<Product> = 
             InitializeComponent();
             //ProductBox.Items = 
         }
