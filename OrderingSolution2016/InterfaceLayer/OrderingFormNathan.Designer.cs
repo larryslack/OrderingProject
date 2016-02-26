25eb8dd0aafab64d43e092f8fcf3393392b3d507
NathanGraf <nathan.graf@mymhc.ca>
2016-02-26 11:40:11 -0700

    Worked on UI for my form and have prodcuts loading into combo box

--- OrderingSolution2016/InterfaceLayer/OrderingFormNathan.Designer.cs
+++ OrderingSolution2016/InterfaceLayer/OrderingFormNathan.Designer.cs
@@ -29,20 +29,32 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.cbCustID = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.lblEmID = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblBaseCost = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbProducts = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
-            this.cbProducts = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.lblBaseCost = new System.Windows.Forms.Label();
+            this.lblCustId = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblReqDate = new System.Windows.Forms.Label();
+            this.lblShipDate = new System.Windows.Forms.Label();
+            this.lblVia = new System.Windows.Forms.Label();
+            this.btnAddOrd = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.lblRawPrice = new System.Windows.Forms.Label();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.lblFin = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -55,14 +67,6 @@
             this.label1.Size = new System.Drawing.Size(65, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Customer ID";
-            // 
-            // cbCustID
-            // 
-            this.cbCustID.FormattingEnabled = true;
-            this.cbCustID.Location = new System.Drawing.Point(120, 32);
-            this.cbCustID.Name = "cbCustID";
-            this.cbCustID.Size = new System.Drawing.Size(121, 21);
-            this.cbCustID.TabIndex = 1;
             // 
             // label2
             // 
@@ -84,6 +88,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.btnDel);
             this.panel1.Controls.Add(this.lblBaseCost);
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
@@ -92,10 +97,68 @@
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.cbProducts);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Location = new System.Drawing.Point(336, 12);
+            this.panel1.Location = new System.Drawing.Point(296, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(626, 318);
+            this.panel1.Size = new System.Drawing.Size(666, 318);
             this.panel1.TabIndex = 4;
+            // 
+            // lblBaseCost
+            // 
+            this.lblBaseCost.AutoSize = true;
+            this.lblBaseCost.Location = new System.Drawing.Point(345, 42);
+            this.lblBaseCost.Name = "lblBaseCost";
+            this.lblBaseCost.Size = new System.Drawing.Size(20, 13);
+            this.lblBaseCost.TabIndex = 7;
+            this.lblBaseCost.Text = "\" \"";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(443, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(43, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(440, 10);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Discount";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(345, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Price per unit";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(231, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Quantity";
+            // 
+            // cbProducts
+            // 
+            this.cbProducts.FormattingEnabled = true;
+            this.cbProducts.Location = new System.Drawing.Point(17, 42);
+            this.cbProducts.Name = "cbProducts";
+            this.cbProducts.Size = new System.Drawing.Size(204, 21);
+            this.cbProducts.TabIndex = 1;
             // 
             // label4
             // 
@@ -108,6 +171,10 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.lblVia);
+            this.panel2.Controls.Add(this.lblShipDate);
+            this.panel2.Controls.Add(this.lblReqDate);
+            this.panel2.Controls.Add(this.lblDate);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Location = new System.Drawing.Point(11, 111);
             this.panel2.Name = "panel2";
@@ -123,74 +190,140 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "Shipping Details";
             // 
-            // cbProducts
+            // lblCustId
             // 
-            this.cbProducts.FormattingEnabled = true;
-            this.cbProducts.Location = new System.Drawing.Point(17, 38);
-            this.cbProducts.Name = "cbProducts";
-            this.cbProducts.Size = new System.Drawing.Size(121, 21);
-            this.cbProducts.TabIndex = 1;
+            this.lblCustId.AutoSize = true;
+            this.lblCustId.Location = new System.Drawing.Point(130, 32);
+            this.lblCustId.Name = "lblCustId";
+            this.lblCustId.Size = new System.Drawing.Size(17, 13);
+            this.lblCustId.TabIndex = 6;
+            this.lblCustId.Text = "\"\"";
             // 
-            // label5
+            // lblDate
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(169, 10);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 13);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "Quantity";
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(14, 54);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(59, 13);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "Order Date";
             // 
-            // label6
+            // lblReqDate
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(281, 10);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(69, 13);
-            this.label6.TabIndex = 3;
-            this.label6.Text = "Price per unit";
+            this.lblReqDate.AutoSize = true;
+            this.lblReqDate.Location = new System.Drawing.Point(92, 54);
+            this.lblReqDate.Name = "lblReqDate";
+            this.lblReqDate.Size = new System.Drawing.Size(50, 13);
+            this.lblReqDate.TabIndex = 2;
+            this.lblReqDate.Text = "ReqDate";
             // 
-            // label7
+            // lblShipDate
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(404, 11);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(49, 13);
-            this.label7.TabIndex = 4;
-            this.label7.Text = "Discount";
+            this.lblShipDate.AutoSize = true;
+            this.lblShipDate.Location = new System.Drawing.Point(14, 79);
+            this.lblShipDate.Name = "lblShipDate";
+            this.lblShipDate.Size = new System.Drawing.Size(51, 13);
+            this.lblShipDate.TabIndex = 3;
+            this.lblShipDate.Text = "ShipDate";
             // 
-            // textBox1
+            // lblVia
             // 
-            this.textBox1.Location = new System.Drawing.Point(172, 40);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(43, 20);
-            this.textBox1.TabIndex = 5;
+            this.lblVia.AutoSize = true;
+            this.lblVia.Location = new System.Drawing.Point(14, 112);
+            this.lblVia.Name = "lblVia";
+            this.lblVia.Size = new System.Drawing.Size(46, 13);
+            this.lblVia.TabIndex = 4;
+            this.lblVia.Text = "Ship Via";
             // 
-            // textBox2
+            // btnAddOrd
             // 
-            this.textBox2.Location = new System.Drawing.Point(407, 40);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(40, 20);
-            this.textBox2.TabIndex = 6;
+            this.btnAddOrd.Location = new System.Drawing.Point(739, 336);
+            this.btnAddOrd.Name = "btnAddOrd";
+            this.btnAddOrd.Size = new System.Drawing.Size(223, 23);
+            this.btnAddOrd.TabIndex = 8;
+            this.btnAddOrd.Text = "Commit";
+            this.btnAddOrd.UseVisualStyleBackColor = true;
             // 
-            // lblBaseCost
+            // btnDel
             // 
-            this.lblBaseCost.AutoSize = true;
-            this.lblBaseCost.Location = new System.Drawing.Point(284, 45);
-            this.lblBaseCost.Name = "lblBaseCost";
-            this.lblBaseCost.Size = new System.Drawing.Size(20, 13);
-            this.lblBaseCost.TabIndex = 7;
-            this.lblBaseCost.Text = "\" \"";
+            this.btnDel.Location = new System.Drawing.Point(555, 35);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(60, 23);
+            this.btnDel.TabIndex = 9;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            // 
+            // lblRawPrice
+            // 
+            this.lblRawPrice.AutoSize = true;
+            this.lblRawPrice.Location = new System.Drawing.Point(333, 374);
+            this.lblRawPrice.Name = "lblRawPrice";
+            this.lblRawPrice.Size = new System.Drawing.Size(17, 13);
+            this.lblRawPrice.TabIndex = 9;
+            this.lblRawPrice.Text = "\"\"";
+            // 
+            // lblTax
+            // 
+            this.lblTax.AutoSize = true;
+            this.lblTax.Location = new System.Drawing.Point(466, 374);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(17, 13);
+            this.lblTax.TabIndex = 10;
+            this.lblTax.Text = "\"\"";
+            // 
+            // lblFin
+            // 
+            this.lblFin.AutoSize = true;
+            this.lblFin.Location = new System.Drawing.Point(578, 374);
+            this.lblFin.Name = "lblFin";
+            this.lblFin.Size = new System.Drawing.Size(17, 13);
+            this.lblFin.TabIndex = 11;
+            this.lblFin.Text = "\"\"";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(333, 341);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Price Before Taxes";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(466, 341);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Tax Rate";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(578, 341);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(110, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Final Cost After Taxes";
             // 
             // OrderingFormNathan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(974, 396);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblFin);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.lblRawPrice);
+            this.Controls.Add(this.lblCustId);
+            this.Controls.Add(this.btnAddOrd);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.lblEmID);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.cbCustID);
             this.Controls.Add(this.label1);
             this.Name = "OrderingFormNathan";
             this.Text = "OrderingFormNathan";
@@ -206,7 +339,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox cbCustID;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblEmID;
         private System.Windows.Forms.Panel panel1;
@@ -220,5 +352,18 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox cbProducts;
+        private System.Windows.Forms.Label lblCustId;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnAddOrd;
+        private System.Windows.Forms.Label lblVia;
+        private System.Windows.Forms.Label lblShipDate;
+        private System.Windows.Forms.Label lblReqDate;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblRawPrice;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Label lblFin;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
--- OrderingSolution2016/InterfaceLayer/OrderingFormNathan.cs
+++ OrderingSolution2016/InterfaceLayer/OrderingFormNathan.cs
@@ -9,20 +9,28 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using BaseLayer;
 using BusinessLayer;
+
 namespace InterfaceLayer
 {
     public partial class OrderingFormNathan : Form
     {
         string CustomerID;
         int EmployeeID = 0;
+
         List<OrderDetail> DetailsList = new List<OrderDetail>();
+        
+        
         public OrderingFormNathan(string CustomerID, int EmployeeID)
         {
             InitializeComponent();
             this.CustomerID = CustomerID;
             this.EmployeeID = EmployeeID;
             lblEmID.Text = EmployeeID.ToString();
-            cbCustID.Text = CustomerID;
+            lblCustId.Text = CustomerID;
+            cbProducts.DataSource = BusinessLayer.Business.ProductList();
+            cbProducts.DisplayMember = "ProductName";
+            cbProducts.ValueMember = "ProductID";
+            
         }
     }
 }
