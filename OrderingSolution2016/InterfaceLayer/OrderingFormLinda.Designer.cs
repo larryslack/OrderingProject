806005ef4fa5b01242b9ee44ffe1c69d5420fee2
larryslack <slack@mhc.ab.ca>
2016-02-26 10:40:09 -0700

    hooking up Linda and Brent forms

---
+++ OrderingSolution2016/InterfaceLayer/OrderingFormLinda.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace InterfaceLayer
+{
+    partial class OrderingFormLinda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCustomerID = new System.Windows.Forms.Label();
+            this.lblCompanyName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCustomerID
+            // 
+            this.lblCustomerID.AutoSize = true;
+            this.lblCustomerID.Location = new System.Drawing.Point(21, 9);
+            this.lblCustomerID.Name = "lblCustomerID";
+            this.lblCustomerID.Size = new System.Drawing.Size(35, 13);
+            this.lblCustomerID.TabIndex = 0;
+            this.lblCustomerID.Text = "label1";
+            // 
+            // lblCompanyName
+            // 
+            this.lblCompanyName.AutoSize = true;
+            this.lblCompanyName.Location = new System.Drawing.Point(121, 9);
+            this.lblCompanyName.Name = "lblCompanyName";
+            this.lblCompanyName.Size = new System.Drawing.Size(35, 13);
+            this.lblCompanyName.TabIndex = 1;
+            this.lblCompanyName.Text = "label1";
+            // 
+            // OrderingFormLinda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 354);
+            this.Controls.Add(this.lblCompanyName);
+            this.Controls.Add(this.lblCustomerID);
+            this.Name = "OrderingFormLinda";
+            this.Text = "OrderingFormLinda";
+            this.Load += new System.EventHandler(this.OrderingFormLinda_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCustomerID;
+        private System.Windows.Forms.Label lblCompanyName;
+    }
+}
---
+++ OrderingSolution2016/InterfaceLayer/OrderingFormLinda.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BaseLayer;
+using BusinessLayer;
+namespace InterfaceLayer
+{
+    public partial class OrderingFormLinda : Form
+    {
+
+        string CustomerID;
+        int EmployeeID;
+        Customer Cust;
+        public OrderingFormLinda(string CustomerID, int EmployeeID)
+        {
+            InitializeComponent();
+            this.CustomerID = CustomerID;
+            this.EmployeeID = EmployeeID;
+            lblCustomerID.Text = CustomerID;
+            List<Customer> Clist = Business.CustomerList();
+            foreach (Customer C in Clist)
+            {
+                if (C.CustomerID == CustomerID)
+                    Cust = C;
+            }
+            lblCompanyName.Text = Cust.CompanyName;
+        }
+
+        private void OrderingFormLinda_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
