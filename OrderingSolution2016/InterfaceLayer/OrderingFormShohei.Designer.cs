800080e21036db789df2faf4d621bd5ef077d623
shoheisaito <Shohei.Saito@mymhc.ca>
2016-03-09 15:20:09 -0700

    MyChange

--- OrderingSolution2016/InterfaceLayer/OrderingFormShohei.Designer.cs
+++ OrderingSolution2016/InterfaceLayer/OrderingFormShohei.Designer.cs
@@ -35,13 +35,33 @@
             this.label2 = new System.Windows.Forms.Label();
             this.lblCust = new System.Windows.Forms.Label();
             this.lblEmpl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ShipList = new System.Windows.Forms.ComboBox();
+            this.txtFreight = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtPost = new System.Windows.Forms.TextBox();
+            this.txtRegion = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.DTRequired = new System.Windows.Forms.DateTimePicker();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.txtShipName = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblOrderID = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnNewP
             // 
-            this.btnNewP.Location = new System.Drawing.Point(154, 6);
+            this.btnNewP.Location = new System.Drawing.Point(217, 6);
             this.btnNewP.Name = "btnNewP";
-            this.btnNewP.Size = new System.Drawing.Size(106, 23);
+            this.btnNewP.Size = new System.Drawing.Size(99, 23);
             this.btnNewP.TabIndex = 0;
             this.btnNewP.Text = "new Product";
             this.btnNewP.UseVisualStyleBackColor = true;
@@ -54,20 +74,22 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlProducts.AutoScroll = true;
             this.pnlProducts.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlProducts.Location = new System.Drawing.Point(154, 35);
+            this.pnlProducts.Location = new System.Drawing.Point(217, 75);
             this.pnlProducts.Name = "pnlProducts";
-            this.pnlProducts.Size = new System.Drawing.Size(307, 257);
+            this.pnlProducts.Size = new System.Drawing.Size(453, 313);
             this.pnlProducts.TabIndex = 1;
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(3, 298);
+            this.button2.Location = new System.Drawing.Point(554, 394);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 57);
+            this.button2.Size = new System.Drawing.Size(110, 61);
             this.button2.TabIndex = 2;
             this.button2.Text = "Order!";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -81,7 +103,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Location = new System.Drawing.Point(13, 38);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(70, 13);
             this.label2.TabIndex = 1;
@@ -99,17 +121,201 @@
             // lblEmpl
             // 
             this.lblEmpl.AutoSize = true;
-            this.lblEmpl.Location = new System.Drawing.Point(88, 55);
+            this.lblEmpl.Location = new System.Drawing.Point(89, 38);
             this.lblEmpl.Name = "lblEmpl";
             this.lblEmpl.Size = new System.Drawing.Size(35, 13);
             this.lblEmpl.TabIndex = 4;
             this.lblEmpl.Text = "label4";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ship via:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Freight:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Shipping address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Shipping City";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 239);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Postal code";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 270);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Region";
+            // 
+            // ShipList
+            // 
+            this.ShipList.FormattingEnabled = true;
+            this.ShipList.Location = new System.Drawing.Point(66, 86);
+            this.ShipList.Name = "ShipList";
+            this.ShipList.Size = new System.Drawing.Size(100, 21);
+            this.ShipList.TabIndex = 11;
+            // 
+            // txtFreight
+            // 
+            this.txtFreight.Location = new System.Drawing.Point(66, 110);
+            this.txtFreight.Name = "txtFreight";
+            this.txtFreight.Size = new System.Drawing.Size(100, 20);
+            this.txtFreight.TabIndex = 12;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(102, 181);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 13;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(102, 207);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 14;
+            // 
+            // txtPost
+            // 
+            this.txtPost.Location = new System.Drawing.Point(102, 236);
+            this.txtPost.Name = "txtPost";
+            this.txtPost.Size = new System.Drawing.Size(100, 20);
+            this.txtPost.TabIndex = 15;
+            // 
+            // txtRegion
+            // 
+            this.txtRegion.Location = new System.Drawing.Point(102, 271);
+            this.txtRegion.Name = "txtRegion";
+            this.txtRegion.Size = new System.Drawing.Size(100, 20);
+            this.txtRegion.TabIndex = 16;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 422);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(74, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Required date";
+            // 
+            // DTRequired
+            // 
+            this.DTRequired.Location = new System.Drawing.Point(91, 416);
+            this.DTRequired.Name = "DTRequired";
+            this.DTRequired.Size = new System.Drawing.Size(200, 20);
+            this.DTRequired.TabIndex = 20;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(17, 300);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(43, 13);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Country";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(102, 297);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(100, 20);
+            this.txtCountry.TabIndex = 22;
+            // 
+            // txtShipName
+            // 
+            this.txtShipName.Location = new System.Drawing.Point(102, 155);
+            this.txtShipName.Name = "txtShipName";
+            this.txtShipName.Size = new System.Drawing.Size(100, 20);
+            this.txtShipName.TabIndex = 23;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(11, 162);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 13);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Ship Name";
+            // 
+            // lblOrderID
+            // 
+            this.lblOrderID.AutoSize = true;
+            this.lblOrderID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrderID.Location = new System.Drawing.Point(88, 56);
+            this.lblOrderID.Name = "lblOrderID";
+            this.lblOrderID.Size = new System.Drawing.Size(21, 20);
+            this.lblOrderID.TabIndex = 26;
+            this.lblOrderID.Text = "--";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(13, 61);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(50, 13);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Order ID:";
+            // 
             // OrderingFormShohei
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 367);
+            this.ClientSize = new System.Drawing.Size(676, 463);
+            this.Controls.Add(this.lblOrderID);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtShipName);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.DTRequired);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtRegion);
+            this.Controls.Add(this.txtPost);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtFreight);
+            this.Controls.Add(this.ShipList);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.lblEmpl);
             this.Controls.Add(this.lblCust);
             this.Controls.Add(this.label1);
@@ -134,5 +340,25 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblCust;
         private System.Windows.Forms.Label lblEmpl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox ShipList;
+        private System.Windows.Forms.TextBox txtFreight;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtPost;
+        private System.Windows.Forms.TextBox txtRegion;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DateTimePicker DTRequired;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.TextBox txtShipName;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblOrderID;
+        private System.Windows.Forms.Label label13;
     }
 }
