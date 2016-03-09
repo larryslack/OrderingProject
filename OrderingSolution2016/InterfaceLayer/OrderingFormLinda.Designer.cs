ce15451d11c32a73e1809c001c73be7f29e3446a
Linda Jada <linda.jada@mymhc.ca>
2016-03-09 12:21:04 -0700

    "Worked on Lindas Order form"

--- OrderingSolution2016/InterfaceLayer/OrderingFormLinda.Designer.cs
+++ OrderingSolution2016/InterfaceLayer/OrderingFormLinda.Designer.cs
@@ -31,7 +31,6 @@
             this.lblCustomerID = new System.Windows.Forms.Label();
             this.lblCompanyName = new System.Windows.Forms.Label();
             this.txtCustName = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -51,6 +50,17 @@
             this.HundredToFiveHundred = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FiveHundredOrMore = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.AdditonalDiscount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -60,15 +70,10 @@
             this.lblCustomerID.AutoSize = true;
             this.lblCustomerID.Location = new System.Drawing.Point(481, 29);
             this.lblCustomerID.Name = "lblCustomerID";
-            this.lblCustomerID.Size = new System.Drawing.Size(65, 13);
+            this.lblCustomerID.Size = new System.Drawing.Size(62, 13);
             this.lblCustomerID.TabIndex = 0;
-<<<<<<< HEAD
-            this.lblCustomerID.Text = "CustomerID:";
+            this.lblCustomerID.Text = "CustomerID";
             this.lblCustomerID.Click += new System.EventHandler(this.lblCustomerID_Click);
-=======
-            this.lblCustomerID.Text = "label1";
-            //this.lblCustomerID.Click += new System.EventHandler(this.lblCustomerID_Click);
->>>>>>> 20b56433973baf6bade776c49c586944faa16d86
             // 
             // lblCompanyName
             // 
@@ -85,13 +90,6 @@
             this.txtCustName.Name = "txtCustName";
             this.txtCustName.Size = new System.Drawing.Size(149, 20);
             this.txtCustName.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(374, 77);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(149, 20);
-            this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
@@ -154,9 +152,9 @@
             this.HundredToFiveHundred,
             this.FiveHundredOrMore,
             this.AdditonalDiscount});
-            this.dataGridView1.Location = new System.Drawing.Point(2, 172);
+            this.dataGridView1.Location = new System.Drawing.Point(2, 213);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(814, 372);
+            this.dataGridView1.Size = new System.Drawing.Size(814, 331);
             this.dataGridView1.TabIndex = 10;
             // 
             // groupBox1
@@ -188,27 +186,27 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(285, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 12;
-            this.label2.Text = "label2";
+            this.label2.Text = "Ship Via:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(13, 122);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(72, 13);
             this.label3.TabIndex = 13;
-            this.label3.Text = "label3";
+            this.label3.Text = "Ship Address:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(285, 123);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(51, 13);
             this.label5.TabIndex = 14;
-            this.label5.Text = "label5";
+            this.label5.Text = "Ship City:";
             // 
             // Product
             // 
@@ -231,19 +229,19 @@
             // FiftyToHundred
             // 
             this.FiftyToHundred.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.FiftyToHundred.HeaderText = "PriceFiftyToHundred";
+            this.FiftyToHundred.HeaderText = "Price50To100";
             this.FiftyToHundred.Name = "FiftyToHundred";
             // 
             // HundredToFiveHundred
             // 
             this.HundredToFiveHundred.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.HundredToFiveHundred.HeaderText = "PriceHundredToFiveHundred";
+            this.HundredToFiveHundred.HeaderText = "Price100To500";
             this.HundredToFiveHundred.Name = "HundredToFiveHundred";
             // 
             // FiveHundredOrMore
             // 
             this.FiveHundredOrMore.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.FiveHundredOrMore.HeaderText = "PriceFiveHundredOrMore";
+            this.FiveHundredOrMore.HeaderText = "Price500OrMore";
             this.FiveHundredOrMore.Name = "FiveHundredOrMore";
             // 
             // AdditonalDiscount
@@ -252,11 +250,111 @@
             this.AdditonalDiscount.HeaderText = "AdditonalDiscount";
             this.AdditonalDiscount.Name = "AdditonalDiscount";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(554, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Ship Country:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(554, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Required Date:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(643, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(285, 162);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Fax:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 165);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Postal Code:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(374, 158);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(149, 20);
+            this.textBox6.TabIndex = 20;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(102, 161);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(149, 20);
+            this.textBox7.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(554, 159);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "Phone:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(643, 155);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(149, 20);
+            this.textBox8.TabIndex = 23;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(374, 76);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(149, 21);
+            this.comboBox1.TabIndex = 25;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(643, 77);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(149, 20);
+            this.dateTimePicker1.TabIndex = 26;
+            // 
             // OrderingFormLinda
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(817, 547);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -265,7 +363,6 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.txtCustName);
             this.Name = "OrderingFormLinda";
             this.Text = "OrderingFormLinda";
@@ -283,7 +380,6 @@
         private System.Windows.Forms.Label lblCustomerID;
         private System.Windows.Forms.Label lblCompanyName;
         private System.Windows.Forms.TextBox txtCustName;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label1;
@@ -303,5 +399,16 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn HundredToFiveHundred;
         private System.Windows.Forms.DataGridViewTextBoxColumn FiveHundredOrMore;
         private System.Windows.Forms.DataGridViewTextBoxColumn AdditonalDiscount;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
--- OrderingSolution2016/InterfaceLayer/OrderingFormLinda.cs
+++ OrderingSolution2016/InterfaceLayer/OrderingFormLinda.cs
@@ -37,7 +37,6 @@ namespace InterfaceLayer
 
         }
 
-<<<<<<< HEAD
         void EditCustomerInfo()
         {
 
@@ -50,15 +49,7 @@ namespace InterfaceLayer
 
         private void lblCustomerID_Click(object sender, EventArgs e)
         {
-=======
-//<<<<<<< HEAD
-//        void EditCustomerInfo()
-//=======
-//        private void lblCustomerID_Click(object sender, EventArgs e)
-//>>>>>>> ea4f5609415680521876e64ab9c1e7934a632d75
-      //{
->>>>>>> 20b56433973baf6bade776c49c586944faa16d86
 
-      //  }
+        }
     }
 }
