520b7184a5df33755415b7a8a4c97dafbb575e81
NathanGraf <nathan.graf@mymhc.ca>
2016-03-11 12:20:22 -0700

    Fixed creating order details.

--- OrderingSolution2016/InterfaceLayer/OrderingFormNathan.Designer.cs
+++ OrderingSolution2016/InterfaceLayer/OrderingFormNathan.Designer.cs
@@ -140,6 +140,7 @@
             this.txtDisc.Name = "txtDisc";
             this.txtDisc.Size = new System.Drawing.Size(40, 20);
             this.txtDisc.TabIndex = 6;
+            this.txtDisc.Text = "0";
             // 
             // txtQuantity
             // 
@@ -147,6 +148,7 @@
             this.txtQuantity.Name = "txtQuantity";
             this.txtQuantity.Size = new System.Drawing.Size(43, 20);
             this.txtQuantity.TabIndex = 5;
+            this.txtQuantity.Text = "0";
             // 
             // label7
             // 
