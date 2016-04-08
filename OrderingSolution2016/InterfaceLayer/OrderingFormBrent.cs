59318c2f482ea35c75121baa6b481eb2ba7a969d
BeepBoopMeow <brent.perner@mymhc.ca>
2016-04-08 14:20:49 -0600

    Brent form further improved

--- OrderingSolution2016/InterfaceLayer/OrderingFormBrent.cs
+++ OrderingSolution2016/InterfaceLayer/OrderingFormBrent.cs
@@ -45,6 +45,12 @@ namespace InterfaceLayer
         {
             if (OrderList.SelectedIndex >= 0)
             {
+                if (BetterNameThanFer[OrderList.SelectedIndex].ProductName == ProductBox.SelectedItem) //If the product being removed is the same as the one selected
+                {
+                    int k = BetterNameThanFer[OrderList.SelectedIndex].Quantity;
+                    InStockBox.Text = (Convert.ToInt16(InStockBox.Text) + k).ToString();
+                    AddBtn.Enabled = true;
+                }
                 BetterNameThanFer.RemoveAt(OrderList.SelectedIndex);
                 OrderList.Items.RemoveAt(OrderList.SelectedIndex);
             }
@@ -54,54 +60,80 @@ namespace InterfaceLayer
         {
             int InStock = Convert.ToInt16(InStockBox.Text);
             //Still need to make it check for multiple things with the same product but different discounts
-            bool EdittingEntry = false;
             if (InStock <= 0)
                 return;
             string AddToOrder = "";
             AddToOrder += ProductBox.SelectedItem.ToString();
             for (int j = 0; j < OrderList.Items.Count; j++)
             {
-                if ((BetterNameThanFer[j].ProductName == ProductBox.SelectedItem.ToString()) && (BetterNameThanFer[j].Discount == Convert.ToSingle(DiscountUpDown.Value)))
+                if (BetterNameThanFer[j].ProductName == ProductBox.SelectedItem.ToString())
                 {
-                    if (BetterNameThanFer[j].Quantity + Convert.ToInt16(QuantityUpdown.Value) <= InStock)
+                    if (BetterNameThanFer[j].Discount == Convert.ToSingle(DiscountUpDown.Value))
                     {
-                        BetterNameThanFer[j].Quantity += Convert.ToInt16(QuantityUpdown.Value);
-                        EdittingEntry = true;
-                        AddToOrder += " (" + BetterNameThanFer[j].Quantity + ")";
-                        if (DiscountUpDown.Value != 0)
+                        if (Convert.ToInt16(QuantityUpdown.Value) <= InStock)
                         {
-                            AddToOrder += " [" + ((DiscountUpDown.Value) * 100).ToString("#") + "% Discount]";
+                            BetterNameThanFer[j].Quantity += Convert.ToInt16(QuantityUpdown.Value);
+                            AddToOrder += " (" + BetterNameThanFer[j].Quantity + ")";
+                            InStockBox.Text = (InStock - (Convert.ToInt16(QuantityUpdown.Value))).ToString();
+                            if (QuantityUpdown.Value > Convert.ToDecimal(InStockBox.Text))
+                            {
+                                QuantityUpdown.Value = Convert.ToDecimal(InStockBox.Text);
+                                if (QuantityUpdown.Value == 0)
+                                    AddBtn.Enabled = false;
+                            }
+                            if (DiscountUpDown.Value != 0)
+                            {
+                                AddToOrder += " [" + ((DiscountUpDown.Value) * 100).ToString("#") + "% Discount]";
+                            }
+                            decimal garb = fer[ProductBox.SelectedIndex].UnitPrice;
+                            garb = garb * (BetterNameThanFer[j].Quantity * (1 - DiscountUpDown.Value));
+                            AddToOrder += " : " + garb.ToString("c");
+                            OrderList.Items[j] = AddToOrder;
+                            decimal TooLazyToRenameThem = fer[ProductBox.SelectedIndex].UnitPrice;
+                            BetterNameThanFer[j].UnitPrice = (TooLazyToRenameThem * BetterNameThanFer[j].Quantity) * (1 - DiscountUpDown.Value);
+                            //TooLazyToRenameThem = TooLazyToRenameThem * QuantityUpdown.Value * (1 - DiscountUpDown.Value);
+                            
+                            return;
                         }
-                        decimal garb = fer[ProductBox.SelectedIndex].UnitPrice;
-                        garb = garb * (BetterNameThanFer[j].Quantity * (1 - DiscountUpDown.Value));
-                        AddToOrder += " : " + garb.ToString("c");
-                        OrderList.Items[j] = AddToOrder;
-                        return;
+                        else
+                            return;
                     }
-                    else
-                        return;
-                    //float Fisher = Convert.ToSingle(DiscountUpDown.Value * 1);
                 }
             }
-            AddToOrder += " (" + QuantityUpdown.Value + ")"; 
+            AddToOrder += " (" + QuantityUpdown.Value + ")";
             if (DiscountUpDown.Value != 0)
             {
-                AddToOrder += " [" + ((DiscountUpDown.Value)*100).ToString("#") + "% Discount]";
+                AddToOrder += " [" + ((DiscountUpDown.Value) * 100).ToString("#") + "% Discount]";
             }
             AddToOrder += " : " + PriceBox.Text;
             OrderList.Items.Add(AddToOrder);
             //Order Detail code
             int Das = ProductBox.SelectedIndex;
+            //Need to make it so the Unit Price is properly calculated before entering it.  Ask Larry if I should do that, or calculate it during the finalize
             OrderDetail ThisPart = new OrderDetail(0, fer[Das].ProductID, fer[Das].UnitPrice, Convert.ToInt16(QuantityUpdown.Value), Convert.ToSingle(DiscountUpDown.Value));
             ThisPart.ProductName = ProductBox.SelectedItem.ToString();
             BetterNameThanFer.Add(ThisPart);
             InStockBox.Text = (InStock - (Convert.ToInt16(QuantityUpdown.Value))).ToString();
+            if (QuantityUpdown.Value > Convert.ToDecimal(InStockBox.Text))
+            {
+                QuantityUpdown.Value = Convert.ToDecimal(InStockBox.Text);
+                if (QuantityUpdown.Value == 0)
+                    AddBtn.Enabled = false;
+            }
         }
 
         private void ProductBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             //string thing = fer.items
-            InStockBox.Text = fer[ProductBox.SelectedIndex].UnitInStock.ToString();
+            short? InStock = fer[ProductBox.SelectedIndex].UnitInStock;
+            for (int j = 0; j < OrderList.Items.Count; j++)
+            {
+                if (BetterNameThanFer[j].ProductName == ProductBox.SelectedItem.ToString())
+                {
+                    InStock -= BetterNameThanFer[j].Quantity;
+                }
+            }
+            InStockBox.Text = InStock.ToString();
             SupplierBox.Text = fer[ProductBox.SelectedIndex].SupplierID.ToString();
             if (InStockBox.Text == "0")
             {
@@ -158,15 +190,19 @@ namespace InterfaceLayer
             {
                 FinalBtn.Text = "Unfinalize";
                 OrderingToShipping(true);
+                decimal TotalCost = 0;
+                //for (int j = 0; j < OrderList.Items.Count; j++)
+                //{
+                //}
                 OrderList.Items.Add("-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -");
-                OrderList.Items.Add("Conglaturation");
+                OrderList.Items.Add("Conglaturation. It cost  " + TotalCost.ToString("c"));
             }
             else
             {
                 FinalBtn.Text = "Finalize";
                 OrderingToShipping(false);
                 OrderList.Items.Remove("-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -");
-                OrderList.Items.Remove("Conglaturation");
+                OrderList.Items.RemoveAt(OrderList.Items.Count-1);
             }
         }
         private void OrderingToShipping(bool DatWay)
